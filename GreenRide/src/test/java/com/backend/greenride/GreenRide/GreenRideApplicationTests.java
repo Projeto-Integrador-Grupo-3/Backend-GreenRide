47166d61e361e6bd574ffe57c61d2e3b68c052e7
Thalita <eutha.lita.as@gmail.com>
2024-12-17 09:46:40 -0300

    Criação backend do projeto GreenRide

---
+++ GreenRide/src/test/java/com/backend/greenride/GreenRide/GreenRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.greenride.GreenRide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
